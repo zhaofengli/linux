eee04f7236893c84738332bc6c13cf9582489904
Christian König <christian.koenig@amd.com>
2021-11-26 17:11:21 +0100

    drm/ttm: stop pruning fences after wait
    This is just abusing internals of the dma_resv object.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211129120659.1815-3-christian.koenig@amd.com
    (cherry picked from commit f8be2c5971f48dacbd0e6daa44184dbb6f283dc4)
    (cherry picked from commit 31836ce90ef6fe38e8da09dfab97b7c5cd4f765c)
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -1124,7 +1124,6 @@ int ttm_bo_wait(struct ttm_buffer_object *bo,
 	if (timeout == 0)
 		return -EBUSY;
 
-	dma_resv_add_excl_fence(bo->base.resv, NULL);
 	return 0;
 }
 EXPORT_SYMBOL(ttm_bo_wait);
