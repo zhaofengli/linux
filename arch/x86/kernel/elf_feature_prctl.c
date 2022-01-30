76a8cd11866810b8725348d06b54218ca89fcefc
Rick Edgecombe <rick.p.edgecombe@intel.com>
2022-01-30 13:18:37 -0800

    x86/cet/shstk: Support wrss for userspace
    For the current shadow stack implementation, shadow stacks contents cannot
    be arbitrarily provisioned with data. This property helps apps protect
    themselves better, but also restricts any potential apps that may want to
    do exotic things at the expense of a little security.
    
    The x86 shadow stack feature introduces a new instruction, wrss, which
    can be enabled to write directly to shadow stack permissioned memory from
    userspace. Allow it to get enabled via the prctl interface.
    
    Only enable the userspace wrss instruction, which allows writes to
    userspace shadow stacks from userspace. Do not allow it to be enabled
    independently of shadow stack, as HW does not support using WRSS when
    shadow stack is disabled.
    
    Prevent shadow stack's from becoming executable to assist apps who want
    W^X enforced. Add an arch_validate_flags() implementation to handle the
    check. Rename the uapi/asm/mman.h header guard to be able to use it for
    arch/x86/include/asm/mman.h where the arch_validate_flags() will be.
    
    >From a fault handler perspective, WRSS will behave very similar to WRUSS,
    which is treated like a user access from a PF err code perspective.
    
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- arch/x86/kernel/elf_feature_prctl.c
+++ arch/x86/kernel/elf_feature_prctl.c
@@ -21,6 +21,8 @@ static int elf_feat_copy_status_to_user(struct thread_shstk *shstk, u64 __user *
 		buf[1] = shstk->base;
 		buf[2] = shstk->size;
 	}
+	if (shstk->wrss)
+		buf[0] |= LINUX_X86_FEATURE_WRSS;
 
 	return copy_to_user(ubuf, buf, sizeof(buf));
 }
@@ -40,6 +42,8 @@ int prctl_elf_feature(int option, u64 arg2)
 		if (arg2 & thread->feat_prctl_locked)
 			return -EPERM;
 
+		if (arg2 & LINUX_X86_FEATURE_WRSS && !wrss_control(false))
+			feat_succ |= LINUX_X86_FEATURE_WRSS;
 		if (arg2 & LINUX_X86_FEATURE_SHSTK && !shstk_disable())
 			feat_succ |= LINUX_X86_FEATURE_SHSTK;
 
@@ -52,6 +56,8 @@ int prctl_elf_feature(int option, u64 arg2)
 
 		if (arg2 & LINUX_X86_FEATURE_SHSTK && !shstk_setup())
 			feat_succ |= LINUX_X86_FEATURE_SHSTK;
+		if (arg2 & LINUX_X86_FEATURE_WRSS && !wrss_control(true))
+			feat_succ |= LINUX_X86_FEATURE_WRSS;
 
 		if (feat_succ != arg2)
 			return -ECANCELED;
