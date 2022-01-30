5e60c8f89d43f193855e3e01aace0c31dde085f5
Rick Edgecombe <rick.p.edgecombe@intel.com>
2022-01-30 13:18:38 -0800

    x86/cpufeatures: Limit shadow stack to Intel CPUs
    Shadow stack is supported on newer AMD processors, but the kernel
    implementation has not been tested on them. Prevent basic issues from
    showing up for normal users by disabling shadow stack on all CPUs except
    Intel until it has been tested. At which point the limitation should be
    removed.
    
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -517,6 +517,14 @@ __setup("nopku", setup_disable_pku);
 
 static __always_inline void setup_cet(struct cpuinfo_x86 *c)
 {
+	/*
+	 * Shadow stack is supported on AMD processors, but has not been
+	 * tested. Only support it on Intel processors until this is done.
+	 * At which point, this vendor check should be removed.
+	 */
+	if (c->x86_vendor != X86_VENDOR_INTEL)
+		setup_clear_cpu_cap(X86_FEATURE_SHSTK);
+
 	if (!cpu_feature_enabled(X86_FEATURE_SHSTK))
 		return;
 
